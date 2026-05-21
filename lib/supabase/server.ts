c8ae5c3abef017dafc31988483254b061e47acb8
zJaviOne <imalex1ne@gmail.com>
2026-05-21 12:05:54 -0600

    fixed supabase keys in /lib/supabase/server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -10,8 +10,8 @@ export async function createClient() {
   const cookieStore = await cookies()
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    "https://cvovjhsjkfbhvqjquwxh.supabase.co",
+    "sb_publishable_yPAxIjXq8FNVmnx--JolZw_-UE2hzX2",
     {
       cookies: {
         getAll() {
